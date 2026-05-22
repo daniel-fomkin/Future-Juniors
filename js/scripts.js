59c71e0082d14f41e07b50375fcb28185a05d38e
25PY <25PY@TECHIN.INTERNAL>
2026-05-22 14:16:15 +0600

    Start burgre menu adding

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+'use strict';
 // Ricard
 
 const languageSelect = document.getElementById("language-select");
@@ -23,3 +24,32 @@ document.addEventListener("click", (e) => {
 })
 
 // Ricard end
+
+//Burger Menu
+const burgerButton = document.querySelector("#burger-button");
+const ul = document.querySelector("header").querySelector("ul");
+
+burgerButton.addEventListener("click", () => {  
+    if(ul.style.display != "flex"){
+        ul.style.display = "flex";
+        ul.style.width = "100%";
+        document.querySelector("body").style.overflow = "hidden";
+
+        burgerButton.style.border = "5px solid black";
+        burgerButton.style.color = "black";
+        burgerButton.style.position = "fixed";
+        burgerButton.style.top = "1.5rem";
+        burgerButton.style.left = "1.5rem";
+    }
+    else{
+        ul.style.display = "none";
+        ul.style.width = "0%";
+        document.querySelector("body").style.overflow = "auto";
+        burgerButton.style.border = "0";
+        burgerButton.style.color = document.querySelector(".info") ? "white" : "#2E2BD0";
+        burgerButton.style.position = "static";
+    }
+});
+
+
+
