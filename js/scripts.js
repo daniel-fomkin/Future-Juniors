989e81b3ec2943b1597ae6455bcf19dc6128b23d
Nojus M <nojus.macijauskas@stud.techin.lt>
2026-05-25 11:56:58 +0500

    Code ++ #29

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 // Ricard
 
-const languageSelect = document.getElementById("language-select");
-const dropdown = document.querySelector(".language-dropdown");
-const currentLanguage = document.getElementById("current-language");
+// Lang button
+
+const languageSelect = document.getElementById("language-select")
+const dropdown = document.querySelector(".language-dropdown")
+const currentLanguage = document.getElementById("current-language")
 
 languageSelect.addEventListener("click", () => {
     dropdown.classList.toggle("active")
@@ -12,14 +14,55 @@ document.querySelectorAll(".lang-btn").forEach(button => {
     button.addEventListener("click", (e) => {
         e.stopPropagation();
         currentLanguage.textContent = button.querySelector(".lang-short").textContent;
-        dropdown.classList.remove("active");
-    });
-});
+        dropdown.classList.remove("active")
+    })
+})
 
 document.addEventListener("click", (e) => {
     if (!languageSelect.contains(e.target)) {
         dropdown.classList.remove("active")
     }
 })
+
+// LAng buttom
+
+// Feadback
+
+const comments = document.querySelectorAll('.comment')
+const prevBtn = document.getElementById('prevBtn')
+const nextBtn = document.getElementById('nextBtn')
+const counter = document.querySelector('.number-one')
+let currentCenter = 1
+let positions = ['left', 'center', 'right']
+
+function updateCards() {
+    comments.forEach((card, index) => {
+        card.classList.remove('left', 'center', 'right')
+        card.classList.add(positions[index])
+    });
+    counter.textContent = `${currentCenter + 1}/${comments.length}`
+}
+
+updateCards()
+
+nextBtn.addEventListener('click', () => {
+    positions.unshift(positions.pop())
+    currentCenter++
+    if(currentCenter >= comments.length){
+        currentCenter = 0
+    }
+    updateCards();
+})
+
+prevBtn.addEventListener('click', () => {
+    positions.push(positions.shift())
+    currentCenter--
+    if(currentCenter < 0){
+        currentCenter = comments.length - 1
+    }
+    updateCards()
+})
+
+// Feadback
 
 // Ricard end
