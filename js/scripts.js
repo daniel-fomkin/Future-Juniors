c0678f32aaab8ca06c0e33e949fe29184ad7ef28
daniel-fomkin <danielfomkin87@gmail.com>
2026-05-25 10:34:34 +0300

    Add burger menu

--- js/scripts.js
+++ js/scripts.js
@@ -27,29 +27,21 @@ document.addEventListener("click", (e) => {
 
 //Burger Menu
 const burgerButton = document.querySelector("#burger-button");
+const burgerMenu = document.querySelector(".burger-menu")
 const ul = document.querySelector("header").querySelector("ul");
+const closeButton = burgerMenu.querySelector("#closeMenu");
 
 burgerButton.addEventListener("click", () => {  
-    if(ul.style.display != "flex"){
-        ul.style.display = "flex";
-        ul.style.width = "100%";
-        document.querySelector("body").style.overflow = "hidden";
-
-        burgerButton.style.border = "5px solid black";
-        burgerButton.style.color = "black";
-        burgerButton.style.position = "fixed";
-        burgerButton.style.top = "1.5rem";
-        burgerButton.style.left = "1.5rem";
-    }
-    else{
-        ul.style.display = "none";
-        ul.style.width = "0%";
-        document.querySelector("body").style.overflow = "auto";
-        burgerButton.style.border = "0";
-        burgerButton.style.color = document.querySelector(".info") ? "white" : "#2E2BD0";
-        burgerButton.style.position = "static";
-    }
+    burgerMenu.style.display = "flex";
+    document.querySelector("body").style.overflow = "hidden";
 });
+
+closeButton.addEventListener("click", () => {
+    burgerMenu.style.display = "none";
+    document.querySelector("body").style.overflow = "auto";
+
+});
+
 
 
 
