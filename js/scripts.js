25459787222fd5ade3f23fee0783514f5f69c882
Nojus M <nojus.macijauskas@stud.techin.lt>
2026-05-29 10:56:56 +0600

    Code ++ #31

--- js/scripts.js
+++ js/scripts.js
@@ -69,23 +69,17 @@ prevBtn.addEventListener('click', () => {
 
 //Tech
 
-const techItems = document.querySelectorAll(".pin-and-text");
+const techItems = document.querySelectorAll(".pin-and-text")
 
 techItems.forEach(item => {
     item.addEventListener("click", (e) => {
-        e.stopPropagation();
         techItems.forEach(el=>{
             el.classList.remove("active")
-        });
+        })
         item.classList.add("active")
     })
 })
 
-document.addEventListener("click", () => {
-    techItems.forEach(item=>{
-        item.classList.remove("active")
-    })
-})
 
 //Tech
 
